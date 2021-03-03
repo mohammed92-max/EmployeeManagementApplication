07198c194e87f055e1116b2348f36ac085b73d01
mohammed92-max <63492636+mohammed92-max@users.noreply.github.com>
2021-03-03 05:46:28 +0530

    ADDED file

---
+++ application.properties.java
@@ -0,0 +1,9 @@
+spring.datasource.driver=com.mysql.jdbc.Driver
+spring.datasource.url=jdbc:mysql://localhost:3307/employeemanagementapplication?useSSL=false
+spring.datasource.username=root
+spring.datasource.password=$#Ua01554000#$
+
+logging.level.org.hibernate.SQL = debug
+spring.jpa.properties.hibernate.dialect = org.hibernate.dialect.MySQL5Dialect
+
+
