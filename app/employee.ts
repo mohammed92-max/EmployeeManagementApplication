13d5c45a34705d4e74e682cb998bb40e733aca0f
mohammed92-max <63492636+mohammed92-max@users.noreply.github.com>
2021-03-03 12:12:11 +0530

    Add files via upload

---
+++ app/employee.ts
@@ -0,0 +1,16 @@
+export class Employee   
+{
+    constructor 
+    (
+        empid : number,
+        firstname : String,
+        lastname : String,
+        address : String,
+        dob : String,
+        mobile : String,
+        city : String     
+    )
+    {
+
+    }
+}
