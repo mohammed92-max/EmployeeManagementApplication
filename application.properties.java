389231d925d43526e8b27a2864e47e3f57014555
mohammed92-max <63492636+mohammed92-max@users.noreply.github.com>
2021-03-03 12:01:49 +0530

    DELETED file

--- application.properties.java
+++
@@ -1,9 +0,0 @@
-spring.datasource.driver=com.mysql.jdbc.Driver
-spring.datasource.url=jdbc:mysql://localhost:3307/employeemanagementapplication?useSSL=false
-spring.datasource.username=root
-spring.datasource.password=$#Ua01554000#$
-
-logging.level.org.hibernate.SQL = debug
-spring.jpa.properties.hibernate.dialect = org.hibernate.dialect.MySQL5Dialect
-
-
